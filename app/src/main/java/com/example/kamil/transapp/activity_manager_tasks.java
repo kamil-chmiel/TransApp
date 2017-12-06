f7312a83e345d57d4301e3fe4be3bfe146223b20
Konrad Adamik <zawias1904@gmail.com>
2017-12-07 00:34:05 +0100

    k

---
+++ app/src/main/java/com/example/kamil/transapp/activity_manager_tasks.java
@@ -0,0 +1,18 @@
+package com.example.kamil.transapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class activity_manager_tasks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager_tasks);
+    }
+
+}
